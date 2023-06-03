6fc4888e4bf71585ec302f86860eba6e6af4e121
GuzalASLAM <132605959+GuzalASLAM@users.noreply.github.com>
2023-06-02 21:35:23 -0400

    Add files via upload
    char v int

---
+++ homework_1.java
@@ -0,0 +1,27 @@
+package homework;
+
+public class homework_1 {
+    public static void main(String[] args) {
+
+     char g = '0';
+     int gg = g;
+
+        System.out.println(gg);
+
+        // 2 sposob
+
+        char u = '0';
+        int uu = Integer.parseInt(String.valueOf(u));
+
+        System.out.println(uu);
+
+        // 3 sposob
+
+        char l = '0';
+        int ll =Character.getNumericValue(l);
+
+        System.out.println(ll);
+
+    }
+
+}
